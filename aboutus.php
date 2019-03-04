2577ee4d6c089cad33f02065beacb0132d85d9d9
Christopher Garrick <clgarrick@live.com>
2019-03-04 14:06:54 -0800

    updated header and added footer

--- aboutus.php
+++ aboutus.php
@@ -25,43 +25,10 @@
 <meta name="viewport" content="width=1100px; initial-scale=.45;width=device-width; user-scalable=1;">
 <meta http-equiv="X-UA-Compatible" content="IE=Edge">
 <meta name="designer" content="templatesintime.com" />
-<link href="http://fonts.googleapis.com/css?family=Simonetta" rel="stylesheet" type="text/css">
-<link rel="stylesheet" type="text/css" href="css/style.css" />
-<!--[if lt IE 9]>
-<link rel="stylesheet" type="text/css" href="css/ie.css" />
-<![endif]-->
-<link rel="stylesheet" href="css/lightbox.css" type="text/css" media="screen" />
-<script src="javascripts/jquery-1.7.2.min.js" type="text/javascript"></script>
-<script src="javascripts/jquery.smooth-scroll.min.js"></script>
-<script src="javascripts/lightbox.js"></script>
-
-<script>
-  jQuery(document).ready(function($) {
-      $('a').smoothScroll({
-        speed: 1000,
-        easing: 'easeInOutCubic'
-      });
-
-      $('.showOlderChanges').on('click', function(e){
-        $('.changelog .old').slideDown('slow');
-        $(this).fadeOut();
-        e.preventDefault();
-      })
-  });
-</script>
-
 </head>
 
 <body>
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6&appId=136262453114493";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-<div id="wrapper">
+
 <?php include 'header.php'; ?> 
 
 <div class="clearsmall"></div>
@@ -76,72 +43,6 @@
 	<p>Based in California's San Joaquin Valley, we serve clients throughout California, 11 Western States and many countries. Our analytical methods are appropriate for arid and semi-arid agricultural conditions. University of California research is the basis of consultant information.</p>
 	<p>An approved laboratory for the California Department of Food and Agriculture and the California Regional Water Quality Control Board, Dellavalle also holds a California State Water Resources Control Board ELAP certification for water and wastewater analyses.</p>
 <!-- #EndEditable -->
-
-<article class="contentBox2 marginLeft">
-<!-- #BeginEditable "contentBottomL" --> 
-<p>
-&nbsp;</p>
-<!-- #EndEditable -->
-</article>
-
-<article class="contentBox2 marginRight">
-<!-- #BeginEditable "contentBottomR" --> 
-<p>
-&nbsp;</p>
-<!-- #EndEditable -->
-</article>
-<div class="clearsmall"></div>
-</section>
-
-<div class="push"></div>
-</div><!-- ends wrapper -->
-<div id="footer-outer">
-<footer>
-<!-- bottom 3 block area -->
-	<section class="contentBox3">
-<h3>Laboratory:</h3>
-	<h6 class="auto-style1"><strong>Fresno:</strong></h3>
-	<p>1910 W. McKinley Avenue, Suite 110,<br />
-	Fresno, CA 93728<br />
-	1-800-228-9896</p>
-	<p>(559) 233-6129</p>
-	<p>	&nbsp;</p>
-	<p>Hours: Mon - Fri 7am - 5pm</p>
-	<br />
-		<p class="center">	
-		<a href="https://twitter.com/calisoils" target="_blank">
-		<img height="100" src="images/twitter-footer.png" width="275"></a></p>
-</section>
-
-<section class="contentBox3">
-<h3>Consulting Offices:</h3>
-	<h6 class="auto-style1"><strong>Hanford:</strong></h6>
-	<p>1216 N Douty Street<br>Hanford, CA 93230</p>
-	<p>	&nbsp;</p>
-	<h6 class="auto-style1"><strong>Davis</strong></h6>
-	<p>502 Mace Boulevard, #2-B
-	<br>Davis, CA 95618<br></p>
-	<p>&nbsp;</p>
-	<div class="fb-page" data-href="https://www.facebook.com/dellavallelab" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/dellavallelab"><a href="https://www.facebook.com/dellavallelab">Dellavalle Laboratory, Inc.</a></blockquote></div>
-	</div>
-</section>
-	
-<section class="contentBox3">
-<h3>Drop Off Location:</h3>
-<h6 class="auto-style1"><strong>Paso Robles:</strong></h6>
-	<p>
-	3850 Ramada Drive, Suite. A1
-	<br>Paso Robles, CA</p>
-	<p>Drop off Hours: Mon - Fri 7:30 AM - 4:30 PM<br><br>Water Sampling by Appointment only<br>Call Pamela 
-	(559) 647-8895<br>No Consultation Available at This Location<br>Please call 
-	(800) 228-9896 for assistance.</p>
-</section>
-<div class="clearsmall"></div>
-
-	<p class="tiny">Dellavalle Laboratory, Inc. © 	<script type="text/javascript">document.write(new Date().getFullYear());</script></p>
-	<p class="tiny">All Rights Reserved</p>
-<p class="tiny">Designed by <a href="http://www.wattleweb.com" target="_blank">Wattleweb Global Solutions</a></p>
-</footer></div>
 
 </body>
 
---
+++ footer.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<article class="contentBox2 marginLeft">
+<!-- #BeginEditable "contentBottomL" --> 
+<p>
+&nbsp;</p>
+<!-- #EndEditable -->
+</article>
+
+<article class="contentBox2 marginRight">
+<!-- #BeginEditable "contentBottomR" --> 
+<p>
+&nbsp;</p>
+<!-- #EndEditable -->
+</article>
+<div class="clearsmall"></div>
+</section>
+
+<div class="push"></div>
+</div><!-- ends wrapper -->
+<div id="footer-outer">
+<footer>
+<!-- bottom 3 block area -->
+	<section class="contentBox3">
+<h3>Laboratory:</h3>
+	<h6 class="auto-style1"><strong>Fresno:</strong></h3>
+	<p>1910 W. McKinley Avenue, Suite 110,<br />
+	Fresno, CA 93728<br />
+	1-800-228-9896</p>
+	<p>(559) 233-6129</p>
+	<p>	&nbsp;</p>
+	<p>Hours: Mon - Fri 7am - 5pm</p>
+	<br />
+		<p class="center">	
+		<a href="https://twitter.com/calisoils" target="_blank">
+		<img height="100" src="images/twitter-footer.png" width="275"></a></p>
+</section>
+
+<section class="contentBox3">
+<h3>Consulting Offices:</h3>
+	<h6 class="auto-style1"><strong>Hanford:</strong></h6>
+	<p>1216 N Douty Street<br>Hanford, CA 93230</p>
+	<p>	&nbsp;</p>
+	<h6 class="auto-style1"><strong>Davis</strong></h6>
+	<p>502 Mace Boulevard, #2-B
+	<br>Davis, CA 95618<br></p>
+	<p>&nbsp;</p>
+	<div class="fb-page" data-href="https://www.facebook.com/dellavallelab" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/dellavallelab"><a href="https://www.facebook.com/dellavallelab">Dellavalle Laboratory, Inc.</a></blockquote></div>
+	</div>
+</section>
+	
+<section class="contentBox3">
+<h3>Drop Off Location:</h3>
+<h6 class="auto-style1"><strong>Paso Robles:</strong></h6>
+	<p>
+	3850 Ramada Drive, Suite. A1
+	<br>Paso Robles, CA</p>
+	<p>Drop off Hours: Mon - Fri 7:30 AM - 4:30 PM<br><br>Water Sampling by Appointment only<br>Call Pamela 
+	(559) 647-8895<br>No Consultation Available at This Location<br>Please call 
+	(800) 228-9896 for assistance.</p>
+</section>
+<div class="clearsmall"></div>
+
+	<p class="tiny">Dellavalle Laboratory, Inc. © 	<script type="text/javascript">document.write(new Date().getFullYear());</script></p>
+	<p class="tiny">All Rights Reserved</p>
+<p class="tiny">Designed by <a href="http://www.wattleweb.com" target="_blank">Wattleweb Global Solutions</a></p>
+</footer></div>
