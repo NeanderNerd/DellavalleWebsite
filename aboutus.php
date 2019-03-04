dcade5f713f1f23578d04b38433d50966e780ae9
Christopher Garrick <clgarrick@live.com>
2019-03-04 13:58:50 -0800

    make left panal separate

--- aboutus.php
+++ aboutus.php
@@ -66,59 +66,7 @@
 
 <div class="clearsmall"></div>
 
-<aside id="sidebarLeft">
-<h3>Browse Our Site</h3>
-<!-- Start css3menu.com BODY section -->
-<ul id="css3menu1" class="topmenu">
-	<li class="topmenu"><a href="index.html" style="width:156px;">Home</a></li>
-	<li class="topmenu"><a href="aboutus.html" style="width:156px;"><span>About Us</span></a>
-	<ul>
-		<li><a href="consultants.html">Consultants</a></li>
-		<li><a href="clientservices.html">Client Services</a></li>
-		<li><a href="certifiedsamplers.html">Certified Samplers</a></li>
-	</ul></li>
-	<li class="topmenu"><a href="whatwedo.html" style="width:156px;">What We Do</a></li>
-	<li class="topmenu"><a href="services.html" style="width:156px;"><span>Services</span></a>
-	<ul>
-		<li><a href="laboratory.html">Laboratory</a></li>
-		<li><a href="consulting.html">Consulting</a></li>
-		<li><a href="expertise.html">Other Expertise</a></li>
-	</ul></li>
-	<li class="topmenu"><a href="analyticalcatalog.html" style="width:156px;">Analytical Catalog</a></li>
-	<li class="topmenu"><a href="samplingguide.html" style="width:156px;"><span>Sampling Guide</span></a>
-	<ul>
-		<li><a href="sampling_soil.html"><span>Soil</span></a>
-		<ul>
-			<li><a href="sampling-soil-field-and-row-crops.html">Field &amp; Row Crops</a></li>
-			<li><a href="sampling-soil-permanent-crops.html">Permanent Crops</a></li>
-			<li><a href="sampling-soil-ornamentals.html">Ornamental</a></li>
-		</ul></li>
-		<li><a href="sampling_tissue.html"><span>Tissue</span></a>
-		<ul>
-			<li><a href="sampling-tissue-field-and-row-crops.html">Field &amp; Row Crops</a></li>
-			<li><a href="sampling-tissue-permanent-crops.html">Permanent Crops</a></li>
-			<li><a href="sampling-tissue-ornamentals.html">Ornamentals</a></li>
-		</ul></li>
-		<li><a href="sampling-crop-removal.html">Crop Removal</a></li>
-		<li><a href="sampling-water.html"><span>Water</span></a>
-		<ul>
-			<li><a href="sampling-water-irrigation-water-collection.html">Irrigation</a></li>
-			<li><a href="sampling-water-drinking-and-wastewater.html">Drinking &amp; Wastewater</a></li>
-		</ul></li>
-	</ul></li>
-	<li class="topmenu"><a href="accreditation.html" style="width:156px;">Accreditation</a></li>
-	<li class="topmenu"><a href="employment.html" style="width:156px;">Employment</a></li>
-	<li class="topmenu"><a href="forms.html" style="width:156px;">Forms</a></li>
-	<li class="topmenu">
-	<a href="shipping-instructions.html" style="width:156px;">Shipping Instructions</a></li>
-</ul>
-<!-- End css3menu.com BODY section -->
-<!-- #BeginEditable "sidebarLeft" --> 
-
-<p>&nbsp;</p>
-
-<!-- #EndEditable -->
-</aside>
+<?php include 'leftMenu.php'; ?>
 
 <section id="contentRight">
 <!-- #BeginEditable "contentRight" --> 
