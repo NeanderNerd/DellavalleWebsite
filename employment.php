d8305a3ddb7dc5ab1d4cd133b790052903cb10a8
Christopher Garrick <clgarrick@live.com>
2019-04-08 11:54:39 -0700

    minor updates to employment page

--- employment.php
+++ employment.php
@@ -35,13 +35,13 @@
             <!-- #BeginEditable "contentRight" -->
             <h1>Employment</h1>
             <h3>Seasonal Part-Time</h3>
-            <p>We are looking for hardworking and enthusiastic people to join our team. To inquire about these
+            <p>We are looking for hardworking and enthusiastic people to join our team. To inquire about these entry-level
                 opportunities, please contact the Fresno Office and ask for Monica Romero at: (559) 233-6129</p>
             <p>You may also submit a resume (format: Word.doc) to
-                <a href="mailto:pmiller@dellavallelab.com?subject=Employment">m.romero@dellavallelab.com</a>. Or, submit
+                <a href="mailto:m.romero@dellavallelab.com?subject=Employment">m.romero@dellavallelab.com</a>. Or, submit
                 your resume to the attention of Monica Romero at:</p>
             <p>Dellavalle Laboratory, Inc.<br>1910 W. McKinley Avenue, Suite #110<br>Fresno, California 93728<br>Fax:
-                (559) 223-2236
+                (559) 268-8174
             </p>
             <!-- #EndEditable -->
 
