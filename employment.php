b73f1997f8a6c9917ab5f2c117f4fdc065e73649
Christopher Garrick <clgarrick@live.com>
2019-04-09 14:18:33 -0700

    final employment changes

--- employment.php
+++ employment.php
@@ -34,18 +34,16 @@
         <section id="contentRight">
             <!-- #BeginEditable "contentRight" -->
             <h1>Employment</h1>
-            <h3>Seasonal - Temporary - Part-Time</h3>
             <p>
-                We are looking for hardworking and enthusiastic people to join our team. To inquire about these entry-level
-                opportunities, You may submit a resume (format: Word.doc) to
-                <a href="mailto:m.romero@dellavallelab.com?subject=Employment">m.romero@dellavallelab.com</a>. Or, submit
-                your resume to the attention of 'HR' via Fax or Mail at:
+                We are looking for hardworking and enthusiastic people to join our team. To inquire about the seasonal/temporary entry-level
+                opportunities, You may submit your resume (format: Word.doc) by Email to
+                <a href="mailto:m.romero@dellavallelab.com?subject=Employment">m.romero@dellavallelab.com</a>, or by Fax to (559) 268-8174,
+                or by mail to:
             </p>
             <p>
                 Dellavalle Laboratory, Inc.
                 <br>1910 W. McKinley Avenue, Suite #110
                 <br>Fresno, California 93728
-                <br>Fax: (559) 268-8174
             </p>
             <!-- #EndEditable -->
 
