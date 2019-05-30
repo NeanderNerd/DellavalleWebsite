1842480ee369098f6272307a0b64c204a47cb7c4
Christopher Garrick <clgarrick@live.com>
2019-05-30 12:59:21 -0700

    Revert "favicon play"
    This reverts commit 42d052b70256d2100e116b80cf2542978dae131b.

--- PageWrappers/Head.php
+++ PageWrappers/Head.php
@@ -2,6 +2,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.00">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
     <meta name="designer" content="Christopher L. Garrick" />
+    <link rel="shortcut icon" href="favicon.ico" type="image/vnd.microsoft.icon">
     <meta http-equiv="cache-control" content="no-cache" />
     <meta http-equiv="pragma" content="no-cache" />
 
@@ -22,15 +23,6 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
     <link href="http://fonts.googleapis.com/css?family=Simonetta" rel="stylesheet" type="text/css">
-    <!-- Style points -->
-    <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
-    <link rel="manifest" href="images/site.webmanifest">
-    <link rel="mask-icon" href="images/safari-pinned-tab.svg" color="#5bbad5">
-    <link rel="shortcut icon" type="image/png" href="images/favicon-16x16.png">
-    <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="theme-color" content="#ffffff">
     <!--[if lt IE 9]>
     <link rel="stylesheet" type="text/css" href="css/ie.css" />
     <![endif]-->
