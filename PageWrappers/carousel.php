488c0c4ed5f195fae8b6dd9d09430c3973c4e92e
Christopher Garrick <clgarrick@live.com>
2019-05-31 16:10:14 -0700

    favicon fun

--- PageWrappers/carousel.php
+++ PageWrappers/carousel.php
@@ -3,16 +3,24 @@
                 <li data-target="#headerCarousel" data-slide-to="0" class="active"></li>
                 <li data-target="#headerCarousel" data-slide-to="1"></li>
                 <li data-target="#headerCarousel" data-slide-to="2"></li>
+                <li data-target="#headerCarousel" data-slide-to="3"></li>
+                <li data-target="#headerCarousel" data-slide-to="4"></li>
             </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="images/Header.PNG" class="d-block w-100" alt="Header Image">
+                    <img src="images/LogoAlpha(1).png" class="d-block w-100 mx-auto" alt="Dellavalle Logo" data-toggle="tooltip" data-placement="top" title="Dellavalle">
                 </div>
                 <div class="carousel-item">
-                    <img src="images/tractor.jpg" class="d-block w-100" alt="Tractor on Field">
+                    <img src="images/backyard-blur-close-up-296230.jpg" class="d-block w-100 mx-auto" alt="Shovel" data-toggle="tooltip" data-placement="top" title="We Test soil for the optimal chemical makeup.">
                 </div>
                 <div class="carousel-item">
-                    <img src="images/food-and-farming.jpg" class="d-block w-100" alt="Food and Farming">
+                    <img src="images/berries-blur-blurry-39351.jpg" class="d-block w-100 mx-auto" alt="Grapes on Vine" data-toggle="tooltip" data-placement="top" title="We can help with incresing yields.">
+                </div>
+                <div class="carousel-item">
+                    <img src="images/berries-close-up-colors-1125121.jpg" class="d-block w-100 mx-auto" alt="Strawberries on Vine" data-toggle="tooltip" data-placement="top" title="We can optimize your fruit sizes.">
+                </div>
+                <div class="carousel-item">
+                    <img src="images/carabao-clouds-cropland-460229.jpg" class="d-block w-100 mx-auto" alt="Cow on Wetland" data-toggle="tooltip" data-placement="top" title="Well, irrigation, and faucet water testing available.">
                 </div>
             </div>
             <a class="carousel-control-prev" href="#headerCarousel" role="button" data-slide="prev">
