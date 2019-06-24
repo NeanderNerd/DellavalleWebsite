f78f06b8b12e9085d6cbdb90a6551bca85cc08ed
Christopher Garrick <clgarrick@live.com>
2019-06-24 13:10:34 -0700

    Updated Employment page

--- employment.php
+++ employment.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-    <!-- #BeginTemplate "Templates/layout1.dwt" -->
+<!-- #BeginTemplate "Templates/layout1.dwt" -->
 
-    <head>
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-136360584-1"></script>
-        <script>
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-136360584-1"></script>
+    <script>
         window.dataLayer = window.dataLayer || [];
 
         function gtag() {
@@ -14,43 +14,35 @@
         gtag('js', new Date());
 
         gtag('config', 'UA-136360584-1');
-        </script>
-        <!-- #BeginEditable "doctitle" -->
-        <title>Employment - Dellavalle Laboratory Inc.</title>
-        <!-- #BeginEditable "description" -->
-        <meta name="description"
-            content="An environmental/agricultural laboratory and consulting firm located in the San Joaquin Valley with clientele worldwide." />
-        <!-- #EndEditable -->
-    </head>
+    </script>
+    <!-- #BeginEditable "doctitle" -->
+    <title>Employment - Dellavalle Laboratory Inc.</title>
+    <!-- #BeginEditable "description" -->
+    <meta name="description" content="An environmental/agricultural laboratory and consulting firm located in the San Joaquin Valley with clientele worldwide." />
+    <!-- #EndEditable -->
+</head>
 
-    <body>
+<body>
 
-        <?php include 'PageWrappers/header.php'; ?>
+    <?php include 'PageWrappers/header.php'; ?>
 
-        <div class="clearsmall"></div>
+    <div class="clearsmall"></div>
 
-        <?php include 'PageWrappers/leftMenu.php'; ?>
+    <?php include 'PageWrappers/leftMenu.php'; ?>
 
-        <section id="contentRight">
-            <!-- #BeginEditable "contentRight" -->
-            <h1>Employment</h1>
-            <p>
-                Our company is seeking hardworking and enthusiastic people to join our team! We currently have seasonal/temporary Field Ag Tech/Sampler positions
-                available in the Hanford and Ceres-Modesto areas. You may submit your resume (format: Word.doc) by Email to
-                <a href="mailto:m.romero@dellavallelab.com?subject=Employment">m.romero@dellavallelab.com</a>, or by Fax to (559) 268-8174,
-                or by mail to:
-            </p>
-            <p>
-                Dellavalle Laboratory, Inc.
-                <br>1910 W. McKinley Avenue, Suite #110
-                <br>Fresno, California 93728
-            </p>
+    <section id="contentRight">
+        <!-- #BeginEditable "contentRight" -->
+        <h1>Employment</h1>
+        <p>
+            Thank you for visiting our employment page. We currently do not have any open positions; however, we are always accepting resum&eacute;s.
+            If you would like to submit your resum&eacute;, you may do so (format: Word.doc or PDF) by emailing:
+            <a href="mailto:m.romero@dellavallelab.com?subject=Employment">m.romero@dellavallelab.com</a>, or via Fax &commat; (559) 233-2236.
             <!-- #EndEditable -->
 
-    </body>
+</body>
 
-    <?php include 'PageWrappers/footer.php'; ?>
+<?php include 'PageWrappers/footer.php'; ?>
 
-    <!-- #EndTemplate -->
+<!-- #EndTemplate -->
 
 </html>
